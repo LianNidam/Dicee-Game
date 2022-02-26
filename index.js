ab2e799d479e5f1a0cadec1506ec98ba495943a4
LianNidam <liannidam04@gmail.com>
2022-02-26 17:22:25 +0200

    Manipulations on CSS

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 
 var randomNamber1= Math.floor( Math.random()*6)+1; //number between 1-6
-
 var randomDiceImage= "dice" +randomNamber1+ ".png"; // dice1.png - dice6.png
+
+var randomImageSource= "images/" + randomDiceImage; //images/dice1.png - images/dice6.png
+
+var image1 = document.querySelectorAll("img")[0];
+image1.setAttribute("src", randomImageSource);
